a1a7f26af133bd2625006c000601216feacb62ef
Christopher S. Aondona <christohybrid185@gmail.com>
2025-09-28 21:49:55 +0100

    clean up out dir

--- out/full/apps/waitlist/src/routes/set-theme.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'react-router'
-import { createThemeAction } from 'remix-themes'
-import { themeSessionResolver } from '@repo/utils/theme.server'
-
-export const loader = () => redirect('/')
-
-export const action = createThemeAction(themeSessionResolver)
