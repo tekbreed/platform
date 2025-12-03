db6c16b572f8cda94ef2ae315dc29cdae71dd319
Christopher S. Aondona <christohybrid185@gmail.com>
2025-12-03 22:23:35 +0100

    ci: fix playwright PORT in CI

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test"
 import "dotenv/config"
 
-const PORT = process.env.PORT! 
+const PORT = process.env.PORT ?? "5173" 
 
 export default defineConfig({
 	testDir: "./tests/e2e",
